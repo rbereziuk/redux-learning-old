2ba64fc437c725a611841bf684f3949fe06c907b
Roman Bereziuk <roma.physicist@gmail.com>
2021-02-10 15:28:39 +0200

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import store from './customStore'
+import store from './store'
 import * as actions from './actions'
 
-store.subscribe(() => console.log('Hi from custom store'))
 store.dispatch(actions.taskAdded('Clean the room'))
