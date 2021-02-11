0d40f57c1fc046d74bfa6357ea9c50c3c69579b6
Roman Bereziuk <roma.physicist@gmail.com>
2021-02-12 00:04:28 +0200

    Create reducer with function from toolkit

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 export const taskAdded = createAction('taskAdded')
 export const taskRemoved = createAction('taskRemoved')
@@ -12,27 +12,21 @@ let lastId = 0
 
 // Reducer => pure function
 // When the app runs redux calls reducer for setup the store
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case taskAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          complete: false,
-        },
-      ]
 
-    case taskRemoved.type:
-      return state.filter(task => task.id !== action.payload.id)
-
-    case taskCompleted.type:
-      return state.map(task =>
-        task.id === action.payload.id ? { ...task, complete: true } : task
-      )
-
-    default:
-      return state
-  }
-}
+// Redux toolkit under the hood uses immer
+//
+export default createReducer([], {
+  // key: value
+  // actions: functions (event => event handler)
+  [taskAdded.type]: (tasks, action) => {
+    tasks.push({
+      id: ++lastId,
+      description: action.payload.description,
+      complete: false,
+    })
+  },
+  [taskCompleted.type]: (tasks, action) => {
+    const index = tasks.find(task => task.id === action.payload.id)
+    tasks[index].complete = true
+  },
+})
