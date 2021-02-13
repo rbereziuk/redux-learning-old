360396713ac09fb9eacb5cfbc0c237af0750b081
Roman Bereziuk <roma.physicist@gmail.com>
2021-02-13 22:45:49 +0200

    Create tasks slice with createSlice function from redux toolkit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import * as actions from './store/tasks'
 const store = configureStore()
 
 store.dispatch(actions.taskAdded({ description: 'Clean the room' }))
+store.dispatch(actions.taskCompleted({ id: 1 }))
+store.dispatch(actions.taskAdded({ description: 'Make a breakfast' }))
+store.dispatch(actions.taskRemoved({ id: 1 }))
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,11 +1,4 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
-
-export const taskAdded = createAction('taskAdded')
-export const taskRemoved = createAction('taskRemoved')
-export const taskCompleted = createAction('taskCompleted')
-
-// Reducer
-// Initial state => []
+import { createSlice } from '@reduxjs/toolkit'
 
 // Temporary placeholder for id
 let lastId = 0
@@ -14,19 +7,28 @@ let lastId = 0
 // When the app runs redux calls reducer for setup the store
 
 // Redux toolkit under the hood uses immer
-//
-export default createReducer([], {
-  // key: value
-  // actions: functions (event => event handler)
-  [taskAdded.type]: (tasks, action) => {
-    tasks.push({
-      id: ++lastId,
-      description: action.payload.description,
-      complete: false,
-    })
-  },
-  [taskCompleted.type]: (tasks, action) => {
-    const index = tasks.find(task => task.id === action.payload.id)
-    tasks[index].complete = true
+const slice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    // actions: functions     (event => event handler)
+    taskAdded: (tasks, action) => {
+      tasks.push({
+        id: ++lastId,
+        description: action.payload.description,
+        completed: false,
+      })
+    },
+    taskCompleted: (tasks, action) => {
+      const index = tasks.findIndex(task => task.id === action.payload.id)
+      tasks[index].completed = true
+    },
+    taskRemoved: (tasks, action) => {
+      console.log('removed')
+      return tasks.filter(task => task.id !== action.payload.id)
+    },
   },
 })
+
+export const { taskAdded, taskCompleted, taskRemoved } = slice.actions
+export default slice.reducer
