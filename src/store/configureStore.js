00f339160dc7828f8aea2fba5d1e88337afba124
Roman Bereziuk <roma.physicist@gmail.com>
2021-02-18 17:45:34 +0200

    Add thunk middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import reducer from './reducer'
 import { logger } from './middleware/logger'
-import { actionFunc } from './middleware/actionFunc'
 import { toast } from './middleware/toast'
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger('Middleware function argument'), actionFunc, toast],
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger('Middleware function argument'),
+      toast,
+    ],
   })
 }
