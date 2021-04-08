f570c60c572cb0e402d02704062a598af517d1ed
Roman Bereziuk <roma.physicist@gmail.com>
2021-04-08 07:56:37 +0300

    Mock axios

--- src/store/__tests__/tasks.test.js
+++ src/store/__tests__/tasks.test.js
@@ -1,12 +1,16 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 import configureStore from '../configureStore'
 import { addTask } from '../tasks'
 
 describe('tasksSlice', () => {
   test('shoud handle the addTask action', async () => {
-    // dispatch(addTask) => store
-    const store = configureStore()
     const task = { description: 'a' }
+    const savedTask = { ...task, id: 1 }
+    const fakeAxios = new MockAdapter(axios)
+    fakeAxios.onPost('/tasks').reply(200, savedTask)
+    const store = configureStore()
     await store.dispatch(addTask(task))
-    expect(store.getState().entities.tasks.list).toHaveLength(1)
+    expect(store.getState().entities.tasks.list).toContainEqual(savedTask)
   })
 })
